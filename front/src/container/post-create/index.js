f13401b7926f4585ad754955c7706e795e2c31bb
unknown <makskanevski12@gmail.com>
2024-04-25 15:36:30 +0200

    create twitter

---
+++ front/src/container/post-create/index.js
@@ -0,0 +1,81 @@
+import { useReducer, memo, useCallback } from "react";
+import "./index.css";
+
+import FieldForm from "../../component/field-form";
+import Grid from "../../component/grid";
+
+import { Alert, LOAD_STATUS, Loader } from "../../component/load";
+
+import {
+  requestInitialState,
+  requestReducer,
+  REQUEST_ACTION_TYPE,
+} from "../util/request";
+
+function Container({ onCreate, placeholder, button, id = null }) {
+  const [state, dispatch] = useReducer(requestReducer, requestInitialState);
+
+  const convertData = useCallback(
+    ({ value }) =>
+      JSON.stringify({
+        text: value,
+        username: "user",
+        postId: id,
+      }),
+    [id]
+  );
+
+  const sendData = useCallback(
+    async (dataToSend) => {
+      dispatch({ type: REQUEST_ACTION_TYPE.PROGRESS });
+
+      try {
+        const res = await fetch("http://localhost:4000/post-create", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: convertData(dataToSend),
+        });
+
+        const data = await res.json();
+
+        if (res.ok) {
+          dispatch({ type: REQUEST_ACTION_TYPE.RESET });
+
+          if (onCreate) onCreate();
+        } else {
+          dispatch({ type: REQUEST_ACTION_TYPE.ERROR, message: data.message });
+        }
+      } catch (error) {
+        dispatch({ type: REQUEST_ACTION_TYPE.ERROR, message: error.message });
+      }
+    },
+    [convertData, onCreate]
+  );
+
+  const handleSubmit = useCallback(
+    (value) => {
+      return sendData({ value });
+    },
+    [sendData]
+  );
+
+  return (
+    <Grid>
+      <FieldForm
+        placeholder={placeholder}
+        button={button}
+        onSubmit={handleSubmit}
+      />
+      {state.status === LOAD_STATUS.ERROR && (
+        <Alert status={state.status} message={state.message} />
+      )}
+      {state.status === LOAD_STATUS.PROGRESS && <Loader />}
+    </Grid>
+  );
+}
+
+export default memo(Container, (prev, next) => {
+  return true;
+});
---
+++ front/src/container/post-item/index.js
@@ -0,0 +1,140 @@
+import Box from "../../component/box";
+
+import Grid from "../../component/grid";
+
+import PostCreate from "../post-create";
+
+import PostContent from "../../component/post-content";
+
+import { Alert, Skeleton } from "../../component/load";
+
+import { getDate } from "../util/getDate";
+import { useState, Fragment, useReducer, useEffect, useCallback } from "react";
+
+import {
+  requestInitialState,
+  requestReducer,
+  REQUEST_ACTION_TYPE,
+} from "../util/request";
+
+export default function Container({ id, username, text, date }) {
+  const [state, dispatch] = useReducer(
+    requestReducer,
+    requestInitialState,
+    (state) => ({ ...state, data: { id, username, text, date, reply: null } })
+  );
+
+  const getData = useCallback(async () => {
+    dispatch({ type: REQUEST_ACTION_TYPE.PROGRESS });
+    try {
+      const res = await fetch(
+        `http://localhost:4000/post-item?id=${state.data.id}`
+      );
+
+      const resData = await res.json();
+
+      if (res.ok) {
+        dispatch({
+          type: REQUEST_ACTION_TYPE.SUCCESS,
+          payload: convertData(resData),
+        });
+      } else {
+        dispatch({
+          type: REQUEST_ACTION_TYPE.ERROR,
+          payload: resData.message,
+        });
+      }
+    } catch (error) {
+      dispatch({
+        type: REQUEST_ACTION_TYPE.ERROR,
+        payload: error.message,
+      });
+    }
+  }, [state.data.id]);
+
+  const convertData = ({ post }) => ({
+    id: post.id,
+    username: post.username,
+    text: post.text,
+    date: getDate(post.date),
+
+    reply: post.reply.reverse().map(({ id, username, text, date }) => ({
+      id,
+      username,
+      text,
+      date: getDate(date),
+    })),
+
+    isEmpty: post.reply.length === 0,
+  });
+
+  const [isOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen === true) {
+      getData();
+    }
+  }, [isOpen]);
+
+  return (
+    <Box style={{ padding: "0" }}>
+      <div
+        style={{
+          padding: "20px",
+          cursor: "pointer",
+        }}
+        onClick={handleOpen}
+      >
+        <PostContent
+          username={state.data.username}
+          date={state.data.date}
+          text={state.data.text}
+        />
+      </div>
+
+      {isOpen && (
+        <div style={{ padding: "0 20px 20px 20px" }}>
+          <Grid>
+            <Box className="post-item__inside-box">
+              <PostCreate
+                placeholder="Post your reply!"
+                button="Reply"
+                id={state.data.id}
+                onCreate={getData}
+              />
+            </Box>
+
+            {state.status === REQUEST_ACTION_TYPE.PROGRESS && (
+              <Fragment>
+                <Box>
+                  <Skeleton />
+                </Box>
+                <Box>
+                  <Skeleton />
+                </Box>
+              </Fragment>
+            )}
+
+            {state.status === REQUEST_ACTION_TYPE.ERROR && (
+              <Alert status={state.status} message={state.message} />
+            )}
+
+            {state.status === REQUEST_ACTION_TYPE.SUCCESS &&
+              state.data.isEmpty === false &&
+              state.data.reply.map((item) => (
+                <Fragment key={item.id}>
+                  <Box>
+                    <PostContent {...item} />
+                  </Box>
+                </Fragment>
+              ))}
+          </Grid>
+        </div>
+      )}
+    </Box>
+  );
+}
